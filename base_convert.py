f16ea6df8e7f61b0583ed96a50339307986aa64a
Amusseus <30673952+Amusseus@users.noreply.github.com>
2021-01-14 18:52:18 -0800

    base converter made

--- base_convert.py
+++ base_convert.py
@@ -1,3 +1,28 @@
+import math
 
+#dictionary to use for numbers with base higher than 10
+number_dict = {
+    10:"A",
+    11:"B",
+    12:"C",
+    13:"D",
+    14:"E",
+    15:"F"
+}
+# int(number in base 10, changed base) -> int(number in different base)
+# changes the number from base 10 to a sepcified base between 2 to 16
 def convert(num, b):
     """Recursive function that returns a string representing num in the base b"""
+    if num==0:
+        return ""
+    next_num = math.floor(num/b)
+    if b> 10 and (num%b)>=10:
+        temp_remainder = number_dict[num%b]
+    else:
+        temp_remainder = num % b
+    return convert(next_num,b) + str(temp_remainder)
+
+print(number_dict[10])
+print(convert(45,2))
+print(convert(30,4))
+print(convert(316,16))
