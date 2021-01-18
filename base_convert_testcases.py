e5780342b046f6b2d335671ec803c87ec8d6c851
Amusseus <30673952+Amusseus@users.noreply.github.com>
2021-01-17 20:39:34 -0800

    updated testcase

--- base_convert_testcases.py
+++ base_convert_testcases.py
@@ -5,17 +5,17 @@ class TestBaseConvert(unittest.TestCase):
 
     def test_base2(self):
         self.assertEqual(convert(45,2),"101101")
-        self.assertEqual(convert(0,2),"")
+        self.assertEqual(convert(0,2),"0")
         self.assertEqual(convert(1000,2),"1111101000")
 
     def test_base4(self):
         self.assertEqual(convert(30,4),"132")
-        self.assertEqual(convert(0,4),"")
+        self.assertEqual(convert(0,4),"0")
         self.assertEqual(convert(1000,4),"33220")
 
     def test_base16(self):
         self.assertEqual(convert(316,16),"13C")
-        self.assertEqual(convert(0,16),"")
+        self.assertEqual(convert(0,16),"0")
         self.assertEqual(convert(1000,16),"3E8")
 
 if __name__ == "__main__":
